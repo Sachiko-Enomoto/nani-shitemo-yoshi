8125a04f3695d3b73147a420c588b865aae61b82
github Yosuke-Kikuchi <yosuke-kikuchi@kccs.co.jp>
2026-06-23 15:20:58 +0900

    Initial commit

---
+++ fy2027-team-project-11/src/main/java/com/example/demo/Fy2027TeamProject11Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fy2027TeamProject11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fy2027TeamProject11Application.class, args);
+	}
+
+}
---
+++ fy2027-team-project-11/src/test/java/com/example/demo/Fy2027TeamProject11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fy2027TeamProject11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
